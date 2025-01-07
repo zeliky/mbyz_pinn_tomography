4d4ba70b047e8c43377ad0689e60c853cfcd8f33
Michael Berman <michael@hac.ac.il>
2025-01-07 13:43:08 +0200

    added time measurements

--- src/main.py
+++ src/main.py
@@ -9,11 +9,12 @@ from models.pinn_unet import MultiSourceTOFModel
 from models.pinn_combined import CombinedSosTofModel
 from training_steps_handlers import TofToSosUNetTrainingStep, TofPredictorTrainingStep, CombinedSosTofTrainingStep
 import os
+import time
+from TimeMeasurement.time_measurement import convert
 from models.eikonal_solver import EikonalSolverMultiLayer
 
 #sos_checkpoint_path = 'pinn_tof-sos_model.5tumors_w_noise.pth'
 #tof_checkpoint_path = 'pinn_tof-predictor_model.sources_only.pth'
-#sos_checkpoint_path = '__pretarained-resnet.pth'
 sos_checkpoint_path = None
 tof_checkpoint_path = None
 combined_checkpoint_path = None
@@ -22,7 +23,7 @@ combined_checkpoint_path = None
 
 def train_sos_predictor():
     global sos_checkpoint_path
-    epochs = 50
+    epochs = 2
     solver = EikonalSolverMultiLayer(num_layers=3, speed_of_sound=1450, domain_size=0.128, grid_resolution=128)
     solver.to('cuda')
     trainer = PINNTrainer(model=TofToSosUNetModel(),
@@ -38,13 +39,15 @@ def train_sos_predictor():
     trainer.train_model()
     log_message(' ')
 
+    trainer.visualize_training_and_validation()
+
     log_message("[main.py] Training pipeline complete.")
 
 
 
 def train_tof_predictor():
     global tof_checkpoint_path
-    epochs = 50
+    epochs = 2
     trainer = PINNTrainer(model=MultiSourceTOFModel(in_channels=1, n_src=32, base_filters=32),
                           training_step_handler=TofPredictorTrainingStep(),
                           batch_size=1,
@@ -63,7 +66,7 @@ def train_tof_predictor():
 
 def train_combined_model():
     global combined_checkpoint_path
-    epochs = 50
+    epochs = 20
     trainer = PINNTrainer(model=CombinedSosTofModel(),
                           training_step_handler=CombinedSosTofTrainingStep(),
                           batch_size=2,
@@ -82,12 +85,19 @@ def train_combined_model():
 
 if __name__ == "__main__":
     # define the terminal_html folder and initiate the corresponding class 'terminal_html'
+    st = time.process_time()
     log_message(' ')
     os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
     log_message("[main.py] Starting PINN training pipeline...")
-
-    train_sos_predictor()
+    print('started')
+    #train_sos_predictor()
     #train_tof_predictor()
-    #train_combined_model()
+    train_combined_model()
+    # Measure time
+    et = time.process_time()
+    res = et - st
+    hours, minutes, seconds = convert(res)
+    log_message(" ")
+    log_message('CPU Execution time: {} hours, {} Minutes, {} seconds'.format(int(hours), int(minutes), int(seconds)))
 
 
